cb1e414d4f8de735fb953d4c1444b118bd5652f9
tokerson <kuba.tokarzewski@gmail.com>
2020-05-13 18:10:16 +0200

    add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,15 @@ import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 import LandingPage from './LandingPage';
-
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
+import { Container } from 'reactstrap';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={LandingPage} />
+        <Container>
+          <Route exact path="/" component={LandingPage} />
+        </Container>
       </Switch>
     );
   }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Button } from 'reactstrap';
 const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page</h1>
+      <Button color="primary">Reactstrap button example</Button>
     </div>
   );
 };
