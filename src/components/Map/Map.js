4c39661ebea0fc1919bfd66abd966ce6b0b94d70
tokerson <kuba.tokarzewski@gmail.com>
2020-05-25 23:05:14 +0200

    Fetch waypoints from backend and convert them to waypoinst for google directions service

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { compose, withProps, lifecycle } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps';
 
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+const API_HOST = process.env.API_HOST || 'http://localhost:8080';
 
 const Map = compose(
   withProps({
@@ -18,22 +20,38 @@ const Map = compose(
     componentDidMount() {
       const DirectionsService = new google.maps.DirectionsService();
 
-      DirectionsService.route(
-        {
-          origin: new google.maps.LatLng(52.226683, 20.948148),
-          destination: new google.maps.LatLng(52.229176, 20.950806),
-          travelMode: google.maps.TravelMode.DRIVING,
-        },
-        (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
-            this.setState({
-              directions: result,
-            });
-          } else {
-            console.error(`error fetching directions ${result}`);
-          }
-        },
-      );
+      let waypoints = [];
+      axios
+        .get(`${API_HOST}/dev/mocked_result`)
+        .then((res) => {
+          console.log(res);
+          if (res.data === undefined) return null;
+          waypoints = res.data.waypoints.map((waypoint) => ({
+            location: { lat: waypoint.location.lat, lng: waypoint.location.lng },
+            stopover: true,
+          }));
+          const origin = waypoints.shift().location;
+          const destination = waypoints.pop().location;
+
+          DirectionsService.route(
+            {
+              origin: origin,
+              destination: destination,
+              travelMode: google.maps.TravelMode.DRIVING,
+              waypoints: waypoints,
+            },
+            (result, status) => {
+              if (status === google.maps.DirectionsStatus.OK) {
+                this.setState({
+                  directions: result,
+                });
+              } else {
+                console.error(`error fetching directions ${result}`);
+              }
+            },
+          );
+        })
+        .catch((err) => console.log(err));
     },
   }),
 )((props) => {
