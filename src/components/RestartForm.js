27d22ff7a9d6efc99ae0dc7d3163a9f0b54c9106
tokerson <kuba.tokarzewski@gmail.com>
2020-05-31 14:06:54 +0200

    Add displaying search parameters

--- src/components/RestartForm.js
+++ src/components/RestartForm.js
@@ -10,33 +10,51 @@ const RestartForm = ({ tripData, onReset }) => {
 
   return (
     <Card style={{ padding: '1rem' }} className="shadow-lg rounded">
-      {console.log(tripData)}
-      <Row>
-        <Col>
-          <h3>Original trip</h3>
-          <br />
-          <p>
-            Original Distance: <b>{formatDistance(tripData.originalDistance)}</b>
-          </p>
-          <p>
-            Original Trip Duration: <b>{formatTime(tripData.originalDuration)}</b>
-          </p>
-          <br />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h3>Proposed trip</h3>
-          <br />
-          <p>
-            Proposed Trip Distance: <b>{formatDistance(tripData.finalDistance)}</b>
-          </p>
-          <p>
-            Proposed Trip Duration: <b>{formatTime(tripData.finalDuration)}</b>
-          </p>
-          <br />
-        </Col>
-      </Row>
+      <h3>Original trip</h3>
+      <br />
+      <p>
+        Original Distance: <b>{formatDistance(tripData.originalDistance)}</b>
+      </p>
+      <p>
+        Original Trip Duration: <b>{formatTime(tripData.originalDuration)}</b>
+      </p>
+      <hr />
+      <h3>Proposed trip</h3>
+      <br />
+      <p>
+        Proposed Trip Distance: <b>{formatDistance(tripData.finalDistance)}</b>
+      </p>
+      <p>
+        Proposed Trip Duration: <b>{formatTime(tripData.finalDuration)}</b>
+      </p>
+      <hr />
+      <h3>Search parameters</h3>
+      <br />
+      <p>
+        Max. time spent in one place: <b>{formatTime(tripData.parameters.timeInPoi)}</b>
+      </p>
+      <p>
+        Min. rating of a plce: <b>{tripData.parameters.minimalRating}</b>
+      </p>
+      <p>
+        Max. time added to trip time: <b>{formatTime(tripData.parameters.additionalTime)}</b>
+      </p>
+      <p>
+        Max. distance added to trip distance:{' '}
+        <b>{formatDistance(tripData.parameters.additionalDistance)}</b>
+      </p>
+      <p>
+        Min. first stop time: <b>{formatTime(tripData.parameters.searchingStart)}</b>
+      </p>
+      <p>
+        Places categories:{' '}
+        <b>
+          {tripData.parameters.categories.length > 0
+            ? tripData.parameters.categories.join(', ')
+            : 'All'}
+        </b>
+      </p>
+      <br />
       <Button onClick={() => onReset()} color="primary">
         Plan trip again
       </Button>
